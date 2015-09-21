d3fffa7622625fcb37a6407e746096768abe59a9
bchaffee <brandon.c.chaffee@gmail.com>
2015-09-20 22:30:06 -0700

    Week 1 Commit

---
+++ Week 1 Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 1 Homework
+//
+//  Created by Brandon Chaffee on 9/20/15.
+//  Copyright © 2015 Brandon Chaffee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
